b93419cfa789df2258aed8f8e4afcd9dfe668153
Peter Krautzberger <p.krautzberger@gmail.com>
2025-08-22 21:57:01 +0200

    ci: first try texml

---
+++ .github/workflows/texml.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo $(pwd)
+
+echo "install dependencies"
+sudo apt install -y texlive \
+ texlive-extra-utils \
+ texlive-xetex \
+ texlive-bibtex-extra \
+ texlive-fonts-extra \
+ libexception-class-perl \
+ libconfig-inifiles-perl \
+ libxml-libxml-perl \
+ libxml-libxslt-perl \
+ libxml-simple-perl \
+ xml-twig-tools \
+ libpng-dev \
+ libjpeg-dev \
+ gcc \
+ make \
+ libimage-info-perl \
+ pdf2svg \
+ liblingua-en-numbers-ordinate-perl \
+
+# clone texml
+git clone git@github.com:AmerMathSoc/texml.git
+cp ./texml/cfg/texml.cfg.template ./texml/cfg/texml.cfg
